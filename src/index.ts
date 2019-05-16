859faca630995d0b7460605b3e41be2c26a4439c
Sam Slotsky <sam@manifold.co>
2019-05-16 10:45:51 -0500

    Use @pika/pack as a dev dependency

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,3 @@ interface StringCompatible {
 
 export const gql = (str: TemplateStringsArray, ...values: StringCompatible[]) =>
   str.reduce((result, s, i) => `${result}${s}${values[i] || ""}`, "");
-
-/*
-const getProduct = (id: StringCompatible) => gql`
-  query {
-    product(id: "${id}") {
-      displayName
-    }
-  }
-`;
-*/
